7c628f5ef4257df20489fe0173c3e07075757f8d
hff2 <hophoebeho@gmail.com>
2021-12-09 22:38:48 +0800

    adding icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { smallImage } from "../util";
+//IMAGES
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -16,6 +23,24 @@ const GameDetail = ({ pathId }) => {
       navigate('/')
     }
   }
+
+  //GET PLATFORM IMAGES
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
 
   //Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
@@ -34,7 +59,11 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms?.map((data) => (
-                    <p key={data.platform.id}>{data.platform.name}</p>
+                    <img
+                      alt={data.platform.name}
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                    ></img>
                   ))}
                 </Platforms>
               </Info>
@@ -66,6 +95,7 @@ const CardShadow = styled(motion.div)`
     position: fixed;
     top: 0;
     left: 0;
+    z-index: 5;
     &::-webkit-scrollbar {
         width: 0.5rem;
     }
@@ -85,6 +115,7 @@ const Detail = styled(motion.div)`
     position: absolute;
     left: 10%;
     color: black;
+    z-index: 10;
     img {
         width: 100%;
     }
